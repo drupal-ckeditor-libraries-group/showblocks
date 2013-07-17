3a7f1105d391c72b79c1577a9265c7757755d1a6
Wiktor Walc <w.walc@cksource.com>
2013-07-17 17:38:41 +0200

    Updated translations, added Indonesian language.

--- lang/id.js
+++ lang/id.js
@@ -2,6 +2,6 @@
 Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
-CKEDITOR.plugins.setLang( 'showblocks', 'ko', {
-	toolbar: '블록 보기'
+CKEDITOR.plugins.setLang( 'showblocks', 'id', {
+	toolbar: 'Perlihatkan Blok'
 });
